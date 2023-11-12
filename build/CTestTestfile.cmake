409784407387d5fd1ee5743e3a3f92d44e78db88
Tim Ruser <tim@ruser.dev>
2023-11-12 14:59:52 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/timru/Documents/cpp/physics
-# Build directory: C:/Users/timru/Documents/cpp/physics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
